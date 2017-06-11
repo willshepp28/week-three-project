430fc22e3537d27dc8932fff34049bd34c16cb6e
willshepp28 <willsheppard28@outlook.com>
2017-06-11 15:32:07 -0400

    deleted console.logs

--- main.js
+++ main.js
@@ -9,13 +9,12 @@ var str = '';
 // add a 'click' event listener to every button
 for (let i = 0; i < button.length; i++) {
 
-  
+
 button[i].addEventListener('click', function(){
 
 
     // if equal value pushed run eval()
     if (button[i].value === "=") {
-        console.log(button[i].value);
       display.innerHTML = eval(str);
 
 
@@ -23,9 +22,7 @@ button[i].addEventListener('click', function(){
       // if c value pushed clear display
     } else if (button[i].value === 'c') {
        str = "";
-       console.log(str);
        display.innerHTML = str;
-
          // else if any buttons with a value of 0-9 is pressed store it in string
     } else {
 
